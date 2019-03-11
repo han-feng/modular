e2662f7ea6ccb2ba81ad69a62f28d1650878b6ff
han_feng <han_feng@foxmail.com>
2019-03-11 13:47:16 +0800

    简化sync-version.js

--- script/sync-version.js
+++ script/sync-version.js
@@ -2,22 +2,9 @@
 // 版本号同步工具
 // 读取 package.json 中 version 的值，更新 sonar-project.properties 中的 sonar.projectVersion 的值
 const fs = require('fs')
-const path = require('path')
 const chalk = require('chalk')
 
-const packagePath = path.resolve(process.cwd(), 'package.json')
-const sonarPath = path.resolve(process.cwd(), 'sonar-project.properties')
-
-if (!fs.existsSync(packagePath)) {
-  console.error(chalk.red(`No package.json file found in' ${process.cwd()} is this a JavaScript repo?`))
-  process.exit(1)
-}
-
-const packageJson = require(packagePath)
-
-if (!packageJson.version) {
-  console.error(chalk.red('package.json has no version field'))
-}
+const sonarPath = 'sonar-project.properties'
 
 if (!fs.existsSync(sonarPath)) {
   console.log(chalk.yellow('skipped: sonar-project.properties'))
@@ -33,7 +20,7 @@ if (!match) {
 }
 
 const oldSonarVersionString = match[0]
-const newSonarVersionString = `sonar.projectVersion=${packageJson.version}`
+const newSonarVersionString = `sonar.projectVersion=${process.env.npm_package_version}`
 if (oldSonarVersionString !== newSonarVersionString) {
   fs.writeFileSync(sonarPath, sonarFile.replace(oldSonarVersionString, newSonarVersionString), 'utf8')
   console.log(chalk.green(`replace '${oldSonarVersionString}' -> '${newSonarVersionString}'`))

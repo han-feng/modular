23485239e2b07986fa0d7ea0e7b35a59aca27640
han_feng <han_feng@foxmail.com>
2019-04-14 09:43:15 +0800

    调整gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,47 @@
-const { dest, series } = require('gulp')
-const ts = require('gulp-typescript')
+const { task, src, dest, series } = require('gulp')
+const plugins = require('gulp-load-plugins')()
 const del = require('del')
 const pump = require('pump')
-const license = require('gulp-license')
+const dotgitignore = require('dotgitignore')()
 
-const project = ts.createProject('tsconfig.json')
+const project = plugins.typescript.createProject('tsconfig.json')
 
-const clean = function () {
-  return del('dist')
-}
-exports.clean = clean
+task('clean', () => del('dist'))
 
-const build = function (callback) {
+task('build', callback => {
   pump([
     project.src(),
     project(),
-    license('MIT', { organization: 'han_feng@foxmail.com' }),
+    plugins.if(
+      file => plugins.match(file, '*.js'),
+      plugins.terser()
+    ),
+    plugins.license('MIT', { organization: 'han_feng@foxmail.com' }),
     dest('dist')
   ], callback)
-}
-exports.build = build
+})
 
-exports.default = series(clean, build)
+// task('jest', callback => {
+//   pump([
+//     src('test'),
+//     plugins.jest.default()
+//   ], callback)
+// })
+
+task('source', callback => {
+  pump([
+    // '**'不包含'.'开头的文件，需要补充'**/.*'
+    src(['**{,/.*}', '!node_modules', '!.git']),
+    // plugins.excludeGitignore(), // 有问题
+    // plugins.gitignore(), // 有问题
+    plugins.filter(file => !dotgitignore.ignore(file.path)),
+    // plugins.filter(file => {
+    //   console.log(file.path)
+    //   return true
+    // }),
+    plugins.zip('source.zip'),
+    dest('dist')
+  ], callback)
+})
+
+task('default', series('clean', 'build'))
